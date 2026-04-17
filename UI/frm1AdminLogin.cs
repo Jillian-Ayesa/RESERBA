d51003398e6181320d84d7c5ec8213608cb06f80
Betot <johnvincentdoria14@gmail.com>
2026-04-17 20:07:13 +0800

    Add project files.

---
+++ UI/frm1AdminLogin.cs
@@ -0,0 +1,15 @@
+namespace RESERBA_DATABASE
+{
+    public partial class frm1AdminLogin : Form
+    {
+        public frm1AdminLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
